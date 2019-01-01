2a30998d64fe487edf49f5c930060095cdc25334
Ei Hirai <e.hirai.0708@gmail.com>
2019-01-02 02:33:12 +0900

    Initial Commit

---
+++ CoreMLTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLTest
+//
+//  Created by 平井瑛 on 2019/01/02.
+//  Copyright © 2019 Ei Hirai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
