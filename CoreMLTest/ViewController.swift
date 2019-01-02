15928be64d283807b09870e3bda36bd0ebb10733
Ei Hirai <ei.nabra.development@gmail.com>
2019-01-03 02:16:32 +0900

    This is first commit on GitHub of CoreMLTestApp

--- CoreMLTest/ViewController.swift
+++ CoreMLTest/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var confidenceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    let resnetModel = Resnet50()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+    }
+    
+    func processImage(image: UIImage) {
+        if let model = try? VNCoreMLModel(for: self.resnetModel.model) {
+            let request = VNCoreMLRequest(model: model) { (request, error) in
+                if let results = request.results as? [VNClassificationObservation] {
+                    for classification in results {
+                        print("ID: \(classification.identifier) Confidence: \(classification.confidence)")
+                    }
+                    self.descriptionLabel.text = results.first?.identifier
+                    if let confidence = results.first?.confidence {
+                        self.confidenceLabel.text = "\(confidence * 100.0)%"
+                    }
+                }
+            }
+            if let imageData = image.pngData() {
+                let handler = VNImageRequestHandler(data: imageData, options: [:])
+                try? handler.perform([request])
+            }
+        }
     }
 
-
+    @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+            processImage(image: selectedImage)
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func trashTapped(_ sender: Any) {
+        imageView.image = nil
+        descriptionLabel.text = "category"
+        confidenceLabel.text = "confidence"
+    }
+    
 }
 
